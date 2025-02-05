664ae3925e22399d297a9b7f6154c0e6b8bdf3a5
Eric Feng <14ericfeng@gmail.com>
2025-02-04 23:30:28 -0500

    first commit

---
+++ ICS4U/src/unit_0_review/A1E2.java
@@ -0,0 +1,14 @@
+package unit_0_review;
+
+public class A1E2 {
+
+	public static void main(String[] args) {
+		//Using print() and println() to display name, address, and school info with surrounding X
+		System.out.println("                                                    XXXXXXXXXXXXXXXXXXXXXXX");
+		System.out.println("                                                    X    Eric Feng        X");
+		System.out.println("                                                    X    123 Street       X");
+		System.out.println("                                                    X    White Oaks SS    X");
+		System.out.println("                                                    XXXXXXXXXXXXXXXXXXXXXXX");
+	}
+
+}
---
+++ ICS4U/src/unit_0_review/MyFirstProgram.java
@@ -0,0 +1,14 @@
+package unit_0_review;
+
+/**
+ * My first program to test out the print commands.
+ * @author Eric Feng
+ * @version Feb 4 2025
+ */
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+	}
+
+}
