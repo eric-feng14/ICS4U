6617730cc68bffe174c8a5d196c5122019fa71a0
Eric Feng <14ericfeng@gmail.com>
2025-02-08 20:31:16 -0500

    add Test.java from desktop

---
+++ ICS4U/src/unit_0_review/Test.java
@@ -0,0 +1,12 @@
+package unit_0_review;
+//import java.util.Scanner;
+import java.text.NumberFormat;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		NumberFormat n = NumberFormat.getCurrencyInstance();
+		System.out.println(n.format(21.5));
+		 }
+}
