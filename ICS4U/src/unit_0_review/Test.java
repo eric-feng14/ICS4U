877606fc8f6a5b4dbfbf124ef57266f427bd9ff5
Eric Feng <14ericfeng@gmail.com>
2025-02-18 21:07:49 -0500

    test file change

--- ICS4U/src/unit_0_review/Test.java
+++ ICS4U/src/unit_0_review/Test.java
@@ -5,23 +5,29 @@ public class Test {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int n = input.nextInt();
-		input.close();
-		switch (n) {
-			case 0: System.out.println("Better luck next time.");
-			case 1:
-			case 2:
-			case 3:
-			case 4:
-			case 5: System.out.println("Pretty good.");
-			case 6:
-			case 7:break;
-			case 8:
-			case 9:
-			case 10: System.out.println("Great!");
-			default:
-				System.out.println("Default statement reachjed");
-				break;
+		int n;
+//		switch (n) {
+//			case 0: System.out.println("Better luck next time.");
+//			case 1:
+//			case 2:
+//			case 3:
+//			case 4:
+//			case 5: System.out.println("Pretty good.");
+//			case 6:
+//			case 7:break;
+//			case 8:
+//			case 9:
+//			case 10: System.out.println("Great!");
+//			default:
+//				System.out.println("Default statement reachjed");
+//				break;
+//		}
+//		do {
+//			System.out.print("Enter a number less than 4: ");
+//			n = input.nextInt();
+//		} while (n >= 4);
+		for (int i = 1; i++ <= 5; i++) {
+			System.out.println(i);
 		}
 	}
 }
