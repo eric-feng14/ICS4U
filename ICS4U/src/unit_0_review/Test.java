1392df3f7addc7371752d22efb8e082b3d05e28d
Eric Feng <14ericfeng@gmail.com>
2025-02-20 23:30:37 -0500

    test  file change

--- ICS4U/src/unit_0_review/Test.java
+++ ICS4U/src/unit_0_review/Test.java
@@ -4,6 +4,15 @@ import java.util.*;
 public class Test {
 
 	public static void main(String[] args) {
+		int n = 123;
+		String line = n + "";
+		java.lang.Integer a = Integer.parseInt("9");
+		System.out.println("integer a's type :" + a.getClass());
+		int value = a.intValue();
+		int value2 = Integer.parseInt("123");
+		System.out.println("int value :" + value);
+		System.out.println("value2:" + value2);
+		
 		
 	}
 }
