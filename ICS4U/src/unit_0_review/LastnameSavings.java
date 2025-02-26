1f64a2e4ad07c4ed88bcda9474e4d412a9e4e5f6
Eric Feng <14ericfeng@gmail.com>
2025-02-25 19:47:08 -0500

    worked on assignment #1

--- ICS4U/src/unit_0_review/LastnameSavings.java
+++ ICS4U/src/unit_0_review/LastnameSavings.java
@@ -3,16 +3,20 @@ import java.util.*;
 
 /**
  * Prints out a savings schedule and grant amount based on user input
- * Notes: the logic when collecting input was a bit buggy. try to fix it tomorrow.
  * @author Eric Feng
  * @version Feb 24 2025
  */
 public class LastnameSavings {
 
 	public static void main(String[] args) {
+		// Declare constants
+		final int maxGrantAmount = 500, maxOverallGrantAmount = 7200, maxContributionTotal = 50000;
+		final double grantPercent = 0.2;
+		
 		// Declare variables
-		int annualContribution, numberOfYears;
-		double annualInterestRate;
+		int annualContribution = 0, numberOfYears = 0;
+		double initialBalance = 0, finalBalance = 0, totalGrantedAmount = 0;
+		double annualInterestRate = 0, currentInterest = 0;
 		boolean inputFinished = false, interestExists = false, contributionExists = false, yearNumExists = false;
 
 		// Continue collecting input until all variables have been initialized
@@ -24,6 +28,7 @@ public class LastnameSavings {
 					System.out.print("Enter the annual contribution amount: ");
 					annualContribution = input.nextInt();
 					if (annualContribution < 0) {
+						System.out.println("Invalid input!");
 						continue;
 					} else {
 						contributionExists = true;
@@ -32,8 +37,9 @@ public class LastnameSavings {
 
 				if (! interestExists) {
 					System.out.print("Enter the annual interest rate (int): ");
-					annualInterestRate = input.nextInt() / 100;
+					annualInterestRate = input.nextDouble() / 100;
 					if (annualInterestRate < 0) {
+						System.out.println("Invalid input!");
 						continue;
 					} else {
 						interestExists = true;
@@ -44,10 +50,21 @@ public class LastnameSavings {
 					System.out.print("Enter the number of years you want to save for: ");
 					numberOfYears = input.nextInt();
 					if (numberOfYears < 0) {
+						System.out.println("Invalid input!");
+						continue;
+					} else if (numberOfYears > 31) {
+						System.out.println("Max number of years is 31");
 						continue;
 					} else {
 						yearNumExists = true;
 					}
+				}
+				
+				if (annualContribution * numberOfYears > maxContributionTotal) {
+					System.out.format("Over the course of %d years, you can contribute a maximum of $%d\n", numberOfYears, maxContributionTotal);
+					contributionExists = false;
+					yearNumExists = false;
+					continue;
 				}
 
 				input.close();
@@ -57,11 +74,23 @@ public class LastnameSavings {
 			}
 		}
 
-		// Perform Calculations
-
-
-		// Output
-
+		// Perform Calculations and output results
+		int currentGrant = Math.min(maxGrantAmount, (int) (grantPercent * annualContribution));
+		System.out.format("Year%3sInitial Balance%3sContribution%3sGrant%3sInterest%3sFinal Balance\n","","","","","","");
+		for (int yr = 1; yr <= numberOfYears; yr++) {
+			if (totalGrantedAmount > maxOverallGrantAmount) {
+				currentGrant = 0;
+			}
+			System.out.println(totalGrantedAmount);
+			initialBalance = finalBalance;
+			finalBalance += annualContribution + currentGrant;
+			currentInterest = annualInterestRate * finalBalance;
+			finalBalance += currentInterest;
+			totalGrantedAmount += currentGrant;
+			System.out.format("%d%21.2f%13.2f%10.2f%10.2f%15.2f\n", yr, (double) initialBalance, (double) annualContribution, (double) currentGrant, currentInterest, (double) finalBalance);
+		}
+		
+		
 	}
 
 }
