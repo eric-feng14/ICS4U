9371ec956ae9f8b985e0c95f4551940296c354d0
Eric Feng <14ericfeng@gmail.com>
2025-02-28 21:41:56 -0500

    small changes

--- ICS4U/src/unit_0_review/Test.java
+++ ICS4U/src/unit_0_review/Test.java
@@ -5,8 +5,26 @@ import java.util.*;
 public class Test {
 
 	public static void main(String[] args) {
-		String test = "test";
-		String sub = test.substring(0,test.length()-1);
-		System.out.println(sub);
+		final int LOW = 'A', HIGH = 'Z';
+		int[] letterCounts = new int[HIGH-LOW+1];
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a word: ");
+		String word = input.nextLine();
+		input.close();
+		
+		char [] wordLetters = word.toUpperCase().toCharArray();
+//		for (int letter = 0; letter < wordLetters.length; letter++ ) {
+//			int offset = wordLetters[letter] - LOW;
+//			letterCounts[offset] += 1;
+//		}
+		for (char i : wordLetters) {
+			int index = i - LOW;
+			letterCounts[index] += 1;
+		}
+		
+		for (int i = LOW; i <= HIGH; i++) {
+			System.out.println((char) i + ": " + letterCounts[i - LOW]);
+		}
 	}
 }
