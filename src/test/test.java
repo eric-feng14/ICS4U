cc47475743cb2d655add1bba6ae45ae0eb3b3084
Eric Feng <14ericfeng@gmail.com>
2025-05-15 14:10:13 -0400

    finished the exercises

--- src/test/test.java
+++ src/test/test.java
@@ -1,19 +1,58 @@
 package test;
 import java.util.*;
-import java.io.*;
+
 public class test {
-	public static void main(String[] args) {
-		
-		try {
-			Scanner input = new Scanner(new File("accounts.txt"));
-			for (int i = 0; i < 3; i++) {
-				int a; double b; String fName, lName;
-				a = input.nextInt(); b = input.nextDouble();
-				fName = input.next(); lName = input.next();
-				System.out.print(a + " " + b + " " + fName+" "+lName+"\n");
+	
+	public static void q1() {
+		String string1 = "tea";
+		String string2 = "eat";
+		for (int i = 0; i < string1.length(); i++) {
+			boolean found = false;
+			for (int j = 0; j < string2.length(); j++) {
+				if (string2.charAt(j) == string1.charAt(i)) {
+					found = true; 
+					string2 = string2.substring(0, j) + string2.substring(j+1);
+					break;
+				}
 			}
-		} catch (FileNotFoundException e) {
-			System.out.println("file not found");
+			if (! found) {
+				System.out.println("impossible");
+				System.exit(0);
+			}
 		}
+		System.out.println("possible");	
+	}
+	
+	public static void q2() {
+		final int SIZE = 52;
+		String string1 = "tea";
+		String string2 = "eat";
+		if (string1.length() != string2.length()) {
+			System.out.println("Not equal length strings! ");
+			System.exit(0);
+		}
+		
+		//Assuming only letters from the alphabet, e.g. a-z, A-Z, the size can be adjusted as necessary
+		int[] freq1 = new int[SIZE];
+		int[] freq2 = new int[SIZE];
+		
+		for (int i = 0; i < string1.length(); i++) {
+			freq1[string1.charAt(i) - 'a']++;
+			freq2[string2.charAt(i) - 'a']++;
+		}
+		
+		for (int i = 0; i < freq1.length; i++) {
+			if (freq1[i] != freq2[i]) {
+				System.out.println("impossible");
+				System.exit(0);
+			}
+		}
+		System.out.println("possible");
+		
+	}
+	
+	public static void main(String[] args) {
+		q1();
+//		q2();
 	}
 }
