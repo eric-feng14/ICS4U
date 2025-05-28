398f2a4dc7fab4779ebfe7086823675f18aec2d2
Eric Feng <14ericfeng@gmail.com>
2025-05-28 11:52:32 -0400

    what

---
+++ src/test/test1.java
@@ -0,0 +1,10 @@
+package test;
+import java.util.*;
+
+public class test1 {
+	public static void main(String[] args) {
+		List<Integer> lst = new ArrayList<Integer>();
+		ArrayList<Integer> otherLst = new ArrayList<Integer>();
+		
+	}
+}
